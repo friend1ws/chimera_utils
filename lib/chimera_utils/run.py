6323ccb70defd099986f79df88e6eb88da616065
friend1ws <friend1ws@gmail.com>
2016-08-16 06:53:20 +0900

    first commit

---
+++ lib/chimera_utils/run.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+def count_main(args):
+    pass
+
---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='intron_retention_utils',
+      version='0.1.0',
+      description='Python tools for processing chimeric reads',
+      author='Yuichi Shiraishi',
+      author_email='friend1ws@gamil.com',
+      url='https://github.com/friend1ws/chimera_utils',
+      package_dir = {'': 'lib'},
+      packages=['chimera_utils'],
+      scripts=['chimera_utils'],
+      license='GPL-3'
+     )
+
