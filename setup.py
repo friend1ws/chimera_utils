6d760d5d3a5cf35513714246126270e87f2e1e4b
friend1ws <friend1ws@gmail.com>
2016-08-24 23:29:08 +0900

    modify version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='intron_retention_utils',
-      version='0.1.0',
+      version='0.1.0beta',
       description='Python tools for processing chimeric reads',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
