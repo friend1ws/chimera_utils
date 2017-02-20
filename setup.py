cc52e1a7df0bf88b71c4128606035415b4eb734b
friend1ws <friend1ws@gmail.com>
2017-02-20 17:29:00 +0900

    modify release version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='intron_retention_utils',
-      version='0.3.0rc1',
+      version='0.3.0',
       description='Python tools for processing chimeric reads',
       author='Yuichi Shiraishi',
       author_email='friend1ws@gamil.com',
