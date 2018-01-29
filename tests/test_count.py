dc83aac97595b86bd111f73524203b9243925ee0
friend1ws <friend1ws@gmail.com>
2018-01-29 09:44:06 +0900

    add count function test

---
+++ tests/test_count.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import unittest
+import os, tempfile, shutil, filecmp
+import chimera_utils 
+from check_download import *
+
+class TestCount(unittest.TestCase):
+
+    def setUp(self):
+        # prepare reference genome
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+
+        check_download("https://storage.googleapis.com/friend1ws_package_data/fusionfusion/MCF-7.Chimeric.out.sam", \
+                       cur_dir + "/resource/star/MCF-7.Chimeric.out.sam")
+ 
+        self.parser = chimera_utils.parser.create_parser()
+
+
+    def test1(self):
+
+        cur_dir = os.path.dirname(os.path.abspath(__file__))
+        tmp_dir = tempfile.mkdtemp()
+
+        star_chimeric_sam = cur_dir + "/resource/star/MCF-7.Chimeric.out.sam"
+        output_dir = tmp_dir 
+ 
+        output_file = tmp_dir + "/MCF-7.Chimeric.count.txt"
+        answer_file = cur_dir + "/data/count/MCF-7.Chimeric.count.txt"
+
+
+        print ' '.join(["count", star_chimeric_sam, output_file])
+        args = self.parser.parse_args(["count", star_chimeric_sam, output_file])
+        args.func(args)
+
+        self.assertTrue(filecmp.cmp(output_file, answer_file, shallow=False))
+
+        shutil.rmtree(tmp_dir)
+
+if __name__ == "__main__":
+    unittest.main()
+
